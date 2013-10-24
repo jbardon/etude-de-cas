27d92e6ce1a6e486888957a4498d920cefc30530
jeremy <jeremy.bardon@etudiant.univ-lr.fr>
2013-10-24 22:50:56 +0200

    Reprise du code avec SDL_fx et bibliothèque Balle

--- src/main/projet.c
+++ src/main/projet.c
@@ -2,7 +2,8 @@
 #include <math.h>
 
 #include <unistd.h> //pause
-#include <CreationEnv.h>
+#include <GestionEnv.h>
+#include <Balle.h>
 
 int main(void){
 
@@ -16,42 +17,30 @@ int main(void){
   cpSpace* space = cpSpaceNew();
   cpVect gravity = cpv(0, -100);  
   cpSpaceSetGravity(space, gravity);
-  
-  CreationEnv_creerPanier(space, ecran, 50);
- 
-  //Caractéristiques physiques de la balle
-  cpFloat radius = 50;
-  cpFloat mass = 1;
-  cpFloat moment = cpMomentForCircle(mass, 0, radius, cpvzero);
-  
-  //Créé une balle dont la position initiale est -150, 50
-  cpBody* ballBody = cpSpaceAddBody(space, cpBodyNew(mass, moment));
-  cpBodySetPos(ballBody, cpv(500, HAUTEUR_ECRAN));
-  
-  //Zone de collision de la balle
-  cpShape* ballShape = cpSpaceAddShape(space, cpCircleShapeNew(ballBody, radius, cpvzero));
-  cpShapeSetFriction(ballShape, 1);
-  
-  //Créé la balle graphique
-  SDL_Ex_Cercle cercle = {500, 0, 50, SDL_Ex_Couleur(255,50,50)};
-  SDL_Ex_DessinerCercle(ecran, &cercle);
-  
+
+  GestionEnv_creerPanier(space, ecran, 50);
+
+  Balle* balle1 = Balle_creer(space, ecran, 100, 0, 50, SDL_MapRGB(ecran->format, 255, 0, 255));
   SDL_Flip(ecran);
     
   //Evolution des objets de l'espace dans le temps
   cpFloat timeStep = 1.0/60.0;
-  for(cpFloat time = 0; time < 20; time += timeStep){
-    cpVect pos = cpBodyGetPos(ballBody);
-    cpVect vel = cpBodyGetVel(ballBody);
-	
-	cpFloat ang = cpBodyGetAngle(ballBody);
-    printf(
+  for(cpFloat time = 0; time < 8; time += timeStep){
+    cpVect pos = Balle_donneCoordonnees(balle1);
+    //cpVect vel = cpBodyGetVel(ballBody);
+    //cpFloat ang = cpBodyGetAngle(ballBody);
+    /*printf(
       "Time is %5.2f. ballBody is at (%5.2f, %5.2f). It's velocity is (%5.2f, %5.2f)\n",
       time, pos.x, pos.y, vel.x, vel.y
+    );*/
+
+    printf(
+      "Time is %5.2f. ballBody is at (%5.2f, %5.2f)\n",
+      time, pos.x, pos.y
     );
-    printf("angle: %f\n",ang);
+
     cpSpaceStep(space, timeStep);
-    SDL_Ex_DeplacerCercle(ecran, &cercle, point(pos.x, HAUTEUR_ECRAN - pos.y), COULEUR_FOND);
+    Balle_deplacer(balle1);
   }
  
   pause();
@@ -61,9 +50,8 @@ int main(void){
   SDL_Quit();
   
   //Détruit l'espace physique
-  cpShapeFree(ballShape);
-  cpBodyFree(ballBody);
-  CreationEnv_supprimerPanier();
+  Balle_supprimer(balle1);
+  GestionEnv_supprimerPanier();
   cpSpaceFree(space);
 
   return 0;
