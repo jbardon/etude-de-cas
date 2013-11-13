758ae42e569d832a9f7bf34cd846bb8423768dc4
jeremy <jeremy.bardon@etudiant.univ-lr.fr>
2013-11-13 21:38:08 +0100

    Maj testRandLettres qui ne fonctionne pas

--- src/GestionEnv/testRandLettres.c
+++ src/GestionEnv/testRandLettres.c
@@ -1,5 +1,11 @@
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#define TAILLE_TAB_LETTRES 26
+#define RATIO_VOYELLES 43,41
 
 static const float lettres [] = {
 									 9.78,
@@ -30,23 +36,71 @@ static const float lettres [] = {
 								   100.00,
 							    };
 
+static char _randLettre(){
+	
+	/**
+	 * Nombre total de lettres générées
+	 */
+	static unsigned int lettresTotal = 0;
+
+	/**
+	 * Nombre de fois que chaque lettre de l'alphabet
+     * a été générée
+	 *
+	 * nb_Lettres permet d'accéder aux statistique comme suit :
+	 *   	nbLettre['A'] => nbLettre['Z']
+	 */
+	static unsigned int _lettresGeneres[26] = {0};
+	static unsigned int* lettresGeneres = _lettresGeneres - 65;
+
+	// Génération d'un nombre réél aléatoire entre 0 et 100
+	const float proba = (rand()%10000)/100.0;
+printf("proba: %2.2f\n", proba);
+
+	// Cherche la lettre à renvoyer en fonction 
+    // du nombre généré
+	char lettre = 0;
+
+	unsigned int i = 0, l = 0;
+	while(lettre == 0 && i < TAILLE_TAB_LETTRES){	
+		l = 65 + i;
+		if(proba <= lettres[i++]){
+			lettre = (char)l;	
+			lettresGeneres[l]++;
+		}
+	}	
+
+	// La proportion de la lettre trouvée par rapport aux nombre
+	// de lettres générés est trop grande
+	// Ou si le ratio voyelles/consonnes n'est pas correct (à 10% près)
+    // ==> il faut générer un nouveau caractère
+
+	float ratioVoyelles = 0.0;
+	ratioVoyelles += lettresGeneres['A'] + lettresGeneres['E'];
+	ratioVoyelles += lettresGeneres['I'] +lettresGeneres['O'] + lettresGeneres['U'];
+	ratioVoyelles /= lettresTotal;
+
+printf("%c %d %2.2f\n",lettre,lettresTotal, ratioVoyelles);
+	if(lettresTotal > 20){
+		if(((lettresGeneres[lettre]/lettresTotal) > lettres[lettre]) 
+	   		|| ratioVoyelles < RATIO_VOYELLES)
+		{
+			lettresGeneres[lettre]--;
+			return _randLettre();	
+		}
+	}
+	lettresTotal++;
+
+	return lettre;
+}
+
 int main(void){
 
 	srand(time(NULL));
-	const float proba = (rand()%10000)/100.0;
-	printf("proba: %2.2f\n", proba);
-
-	const unsigned int sizeLettres =  sizeof(lettres)/sizeof(float);
-	char lettre = 0;
-
-	unsigned int i = 0;
-	while(lettre == 0 && i < sizeLettres){	
-		if(proba <= lettres[i++]){
-			lettre = (char)(65 + i - 1); //à cause du ++ dans le if		
-		}
-	}	
 	
-	printf("lettre: %c\n", lettre);
+	for(int i = 0; i < 25; i++){
+		printf("%c ", _randLettre());
+	}
 
 	return 0;
 }
