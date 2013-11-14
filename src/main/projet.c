619d21de9ebfd9cfe428977d1175ce1f45b4d3fd
jeremy <jeremy.bardon@etudiant.univ-lr.fr>
2013-11-14 18:46:36 +0100

    test 1

--- src/main/projet.c
+++ src/main/projet.c
@@ -1,25 +1,46 @@
-/**
- * @file projet.c
- * @brief Programme principal du jeu
- * @author Jérémy.B
- * @version 1.0
- *
- * Synopsis :
- *   Affiche l'environnement à l'aide de la bibliothèque GestonEnv
- *   Créé et fait tomber des balles qui sont chacune associé à une lettre
- *   Attent que toutes les balles soient immobiles
- *   L'utilisateur trace une ligne en 2 clic pour sélectionner des balles (et donc des lettres)
- *   Le système affiche le mot le plus grand qui peut être constituer avec les lettres sélectionnées
- *   Suppression des balles sélectionnées
- *
- */
-
 #include <stdio.h>
-#include <math.h>
-
 #include <unistd.h> //pause
 #include <GestionEnv.h>
-#include <Balle.h>
+#include <AlgoRecherche.h>
+
+typedef struct
+{
+    char key[SDLK_LAST];
+    int mousex,mousey;
+    int mousexrel,mouseyrel;
+    char mousebuttons[8];
+        char quit;
+} Input;
+
+void UpdateEvents(Input* in)
+{
+
+    SDL_Event event;
+    while(SDL_PollEvent(&event))
+    {
+        switch (event.type)
+        {
+        
+        case SDL_MOUSEMOTION:
+            in->mousex=event.motion.x;
+            in->mousey=event.motion.y;
+            in->mousexrel=event.motion.xrel;
+            in->mouseyrel=event.motion.yrel;
+            break;
+        case SDL_MOUSEBUTTONDOWN:
+            in->mousebuttons[event.button.button]=1;
+            break;
+        case SDL_MOUSEBUTTONUP:
+            in->mousebuttons[event.button.button]=0;
+            break;
+        case SDL_QUIT:
+            in->quit = 1;
+            break;
+        default:
+            break;
+        }
+    }
+}
 
 int main(void){
 
@@ -27,38 +48,59 @@ int main(void){
 	cpSpace* espace = GestionEnv_initChipmunk();
 
 	GestionEnv_creerPanier(espace, ecran);
+	GestionEnv_creerBalles(35);
 
-	Balle* balle1 = Balle_creer(ecran, espace, cpv(100,0), cpvzero, 50, 0x00FF00FFF, 'A');
-	SDL_Flip(ecran);
+/* DEBUT TEST */
 
-	//Evolution des objets de l'espace dans le temps
-	cpFloat timeStep = 1.0/60.0;
-	for(cpFloat time = 0; time < 8; time += timeStep){
-	cpVect pos = Balle_donneCoordonnees(balle1);
-	float ang = Balle_donneAngleDeg(balle1);
-	//cpVect vel = cpBodyGetVel(ballBody);
-	//cpFloat ang = cpBodyGetAngle(ballBody);
-	/*printf(
-	"Time is %5.2f. ballBody is at (%5.2f, %5.2f). It's velocity is (%5.2f, %5.2f)\n",
-	time, pos.x, pos.y, vel.x, vel.y
-	);*/
-
-	printf(
-	"Temps %5.2f balle(%5.2f, %5.2f) angle %f\n",
-	time, pos.x, pos.y, ang
-	);
-
-	cpSpaceStep(espace, timeStep);
-	Balle_deplacer(balle1);
+	do {		
+		GestionEnv_evoluer();
 	}
+	while(!GestionEnv_ballesImmobiles());
+
+	printf("Cliquez dans la fenêtre pour tracer une ligne\n");
+	cpVect coord[2];
+	Input in;
+	int nbClic = 0;
+
+    memset(&in, 0, sizeof(in));
+    while(nbClic < 2){
+        UpdateEvents(&in);
+        if (in.mousebuttons[SDL_BUTTON_LEFT]){
+			pixelColor(ecran, in.mousex, in.mousey, 0x000000FF);
+//printf("Clic %d: %d,%d\n", nbClic, in.mousex, in.mousey);
+            in.mousebuttons[SDL_BUTTON_LEFT] = 0;
+//printf("x: %d, y: %d\n", in.mousex, in.mousey);
+			coord[nbClic] = cpv(in.mousex, in.mousey);
+			nbClic++;			
+        }            
+    }
+
+	char* lettres = GestionEnv_donnerCaracteresLigne(coord[0].x, coord[0].y, coord[1].x, coord[1].y);
+
+	printf("Lettres sélectionnées: %s\n", lettres);
+
+	GHashTable* dico = chargerDico(RES("/AlgoRecherche/dicofinal.txt"));
+
+	for(int i = 0; i < strlen(lettres); i++){
+	  lettres[i] = tolower(lettres[i]);
+	}
+
+
+	char* result = version1(lettres, dico);
+	if(result){
+		printf("Mot trouvé ! %s (%d pts)\n", result, strlen(result));
+	}
+	else {
+		printf("Aucun mot trouvé =(\n");
+	}
+
+/* FIN TEST */
 
 	pause();
 
-	//Détruit la fenêtre
-	Balle_supprimer(balle1);
-	//Détruit l'espace physique
+	GestionEnv_supprimerBalles();
 	GestionEnv_supprimerPanier();
-	
+
 	GestionEnv_quitChipmunk();
 	GestionEnv_quitSDL();
 
