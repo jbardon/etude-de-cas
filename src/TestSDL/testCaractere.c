f7ea204d73ccf8dbf337b6b06b55867b354fdb14
HAY Charly <charly.hay@gmail.com>
2013-12-08 17:18:19 +0000

    testCaractere.c edited online with Bitbucket

--- src/TestSDL/testCaractere.c
+++ src/TestSDL/testCaractere.c
@@ -1,3 +1,17 @@
+/**
+ * @file testCaractere.c
+ * @brief Test sur l'affichage d'un caractère avec SDL_ttf
+ * @author Jérémy.B
+ * @version 1.0
+ * @date 10 novembre 2013
+ *
+ * Initialisation de la TTF
+ * Inclusion de la police "arial"
+ * Affichage d'un caractère avec SDL_ttf
+ * 
+ */
+
+
 #include <stdio.h>
 #include <math.h>
 #include <SDL/SDL.h>
@@ -5,47 +19,49 @@
 #include <SDL/SDL_image.h>
 #include <unistd.h> //pause
 #include <config.h>
-
 #include <stdlib.h>
 
 int main(void){
 
-SDL_Surface *texte = NULL;
-TTF_Font *police = NULL;
-SDL_Rect position;
+	SDL_Surface *texte = NULL;
+	TTF_Font *police = NULL;
+	
+	SDL_Rect position;
 
-SDL_Init(SDL_INIT_VIDEO);
-TTF_Init();
+	SDL_Init(SDL_INIT_VIDEO);
+	TTF_Init();
 
-//Créé la fenêtre de jeu
-SDL_Init(SDL_INIT_VIDEO);
-SDL_Surface* ecran = SDL_SetVideoMode(LARGUEUR_ECRAN, HAUTEUR_ECRAN, SCREEN_BPP, SDL_HWSURFACE);
-SDL_FillRect(ecran, NULL, COULEUR_FOND);
-SDL_WM_SetCaption("Projet étude de cas", NULL);
- 
+	//Créé la fenêtre de jeu
+	SDL_Init(SDL_INIT_VIDEO);
+	SDL_Surface* ecran = SDL_SetVideoMode(LARGUEUR_ECRAN, HAUTEUR_ECRAN, SCREEN_BPP, SDL_HWSURFACE);
+	SDL_FillRect(ecran, NULL, COULEUR_FOND);
+	SDL_WM_SetCaption("Projet étude de cas", NULL);
+	 
 
-if(TTF_Init() == -1)
-{
-    fprintf(stderr, "Erreur d'initialisation de TTF_Init : %s\n", TTF_GetError());
-    exit(EXIT_FAILURE);
-}
+	if(TTF_Init() == -1)
+	{
+		  fprintf(stderr, "Erreur d'initialisation de TTF_Init : %s\n", TTF_GetError());
+		  exit(EXIT_FAILURE);
+	}
 
-SDL_Color couleurNoire = {0, 0, 0};
-police = TTF_OpenFont("arial.ttf", 65);
+	//Paramètres de rendu du texte : couleur et police
+	SDL_Color couleurNoire = {0, 0, 0};
+	police = TTF_OpenFont("arial.ttf", 65);
+	
+	//Paramètrage du texte
+	texte = TTF_RenderText_Blended(police, "T", couleurNoire);
 
-texte = TTF_RenderText_Blended(police, "T", couleurNoire);
+	position.x = 0;
+	position.y = 0;
+	SDL_BlitSurface(texte, NULL, ecran, &position); 
 
-position.x = 0;
-position.y = 0;
-SDL_BlitSurface(texte, NULL, ecran, &position); 
+	position.x = 130;
+	position.y = 120;
+	SDL_BlitSurface(texte, NULL, ecran, &position);
+	SDL_Flip(ecran);
 
-position.x = 130;
-position.y = 120;
-SDL_BlitSurface(texte, NULL, ecran, &position);
-SDL_Flip(ecran);
-
-TTF_CloseFont(police);  //Doit être avant TTF_Quit() 
-TTF_Quit();
+	TTF_CloseFont(police);  //Doit être avant TTF_Quit() 
+	TTF_Quit();
 
   pause();
   
